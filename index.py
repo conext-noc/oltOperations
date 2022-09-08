7ac376728419af438fc8158ceaea785c90cf9773
ricardovera76 <ricardovera71@gmail.com>
2022-09-07 23:28:16 -0400

    fix: fixed some code issues

--- index.py
+++ index.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
             "Which action will be performed? [activate | deactivate] : ")
         olt = input("In Which OLT the action will be performed? [1|2] : ")
         ip = "181.232.180.5" if olt == "1" else "181.232.180.6"
-        list1 = parser("LISTAS/LISTA_DE_CORTE.csv")
-        list2 = parser("LISTAS/LISTA_DE_CLIENTES.csv")
-        actionList = compare(list1, list2)
-        actionList2 = parser(
+        # list1 = parser("LISTAS/LISTA_DE_CORTE.csv")
+        # list2 = parser("LISTAS/LISTA_DE_CLIENTES.csv")
+        # actionList = compare(list1, list2)
+        actionList = parser(
             "LISTAS/OLT1.csv") if olt == "1" else parser("LISTAS/OLT2.csv")
         if (action == "activate"):
             activate(
@@ -33,7 +33,5 @@ if __name__ == "__main__":
             deactivate(
                 actionList, username, password, port, 1, ip)
             # verify(actionList, "{}ResultOLT.txt".format(action))
-        else:
-            print("no")
         # return clients
     main()
