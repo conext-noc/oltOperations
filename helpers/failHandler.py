6db43f798450945dbb4d32401b5e564ba5b77782
Ricardo Vera <ricardovera71@gmail.com>
2022-11-16 16:54:55 -0400

    fix: fixed issues

--- helpers/failHandler.py
+++ helpers/failHandler.py
@@ -5,15 +5,17 @@ failSTR = "Failure: "
 failTp1 = "% "
 endFail = "MARLLM0"
 anotherFail = "The required ONT does not exist"
+failAnother = "WAN port does not exist"
 
 
 def failChecker(value):
     fail1 = check(value, failSTR)
     fail2 = check(value, failTp1)
     fail3 = check(value, anotherFail)
-    if fail1 == None and fail2 == None and fail3 == None:
+    fail4 = check(value, failAnother)
+    if fail1 == None and fail2 == None and fail3 == None and fail4 == None:
         return None
-    elif fail1 != None and fail2 == None and fail3 == None:
+    elif fail1 != None and fail2 == None and fail3 == None and fail4 == None:
         (_, s) = fail1.span()
         end = checkIter(value, endFail)
         maxLen = len(end) - 1
@@ -21,7 +23,7 @@ def failChecker(value):
         reason = value[s:e].replace("\n", "")
         reason = colorFormatter(reason, "fail")
         return reason
-    elif fail1 == None and fail2 != None and fail3 == None:
+    elif fail1 == None and fail2 != None and fail3 == None and fail4 == None:
         (_, s) = fail2.span()
         end = checkIter(value, endFail)
         maxLen = len(end) - 1
@@ -29,6 +31,9 @@ def failChecker(value):
         reason = value[s:e].replace("\n", "")
         reason = colorFormatter(reason, "fail")
         return reason
-    elif fail1 == None and fail2 == None and fail3 != None:
+    elif fail1 == None and fail2 == None and fail3 != None and fail4 == None:
         reason = colorFormatter(anotherFail, "fail")
+        return reason
+    elif fail1 == None and fail2 == None and fail3 == None and fail4 != None:
+        reason = colorFormatter(failAnother, "fail")
         return reason
