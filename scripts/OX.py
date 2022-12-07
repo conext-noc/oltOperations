54a81e3ac6fb23e75d5e035e5907820eb8ba876f
Ricardo Vera <ricardovera71@gmail.com>
2022-12-07 09:31:39 -0400

    fix: fixed bugs

--- scripts/OX.py
+++ scripts/OX.py
@@ -51,7 +51,7 @@ def operate(comm, command, olt, action, quit):
 
     if keep == "Y":
         for client in actionList:
-            NOMBRE = client["NAME"]
+            NOMBRE = client["NOMBRE"]
             FRAME = client["FRAME"]
             SLOT = client["SLOT"]
             PORT = client["PORT"]
