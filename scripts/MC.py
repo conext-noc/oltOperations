62b77dbbf815de6ba0f3dae5bdd73c6bb0327cde
Ricardo Vera <ricardovera71@gmail.com>
2022-12-27 15:28:33 -0400

    add(MC->CP 1): added functionality to change Data plan in new OLT

--- scripts/MC.py
+++ scripts/MC.py
@@ -20,7 +20,8 @@ Que cambio se realizara?
   > (AS)    :   Agregar Service Port
 $ """
     )
-    lookupType = inp("Buscar cliente por serial o por Datos (F/S/P/ID) [S | D] : ")
+    lookupType = inp(
+        "Buscar cliente por serial o por Datos (F/S/P/ID) [S | D] : ")
     data = dataLookup(comm, command, olt, lookupType)
     proceed = display(data, "A")
     if not proceed:
@@ -38,7 +39,7 @@ $ """
                 "success",
             )
         )
-        modify(data["sn"],NEW_NAME,"NAME")
+        modify(data["sn"], NEW_NAME, "NAME")
         quit()
         return
     if action == "CO":
@@ -52,14 +53,15 @@ $ """
                 "success",
             )
         )
-        modify(data["sn"],NEW_SN,"SN")
+        modify(data["sn"], NEW_SN, "SN")
         quit()
         return
     if action == "CP":
         if olt != "1":
             log("| {:^3} | {:^4} | {:^6} |".format("IDX", "VLAN", "SPID"))
             for idx, wan in enumerate(data["wan"]):
-                log("| {:^3} | {:^4} | {:^6} |".format(idx, wan["vlan"], wan["spid"]))
+                log("| {:^3} | {:^4} | {:^6} |".format(
+                    idx, wan["vlan"], wan["spid"]))
             IDX = int(inp("Ingrese el INDEX del service-port a cambiar : "))
             NEW_PLAN = inp("Ingrese el Nuevo Plan del cliente : ")
             NEW_VLAN = inp("Ingrese la Nueva Vlan del cliente : ")
@@ -68,35 +70,30 @@ $ """
             command(
                 f" service-port {data['wan'][IDX]['spid']} vlan {NEW_VLAN} gpon {data['frame']}/{data['slot']}/{data['port']} ont {data['id']} gemport 14 multi-service user-vlan {NEW_VLAN} tag-transform transparent inbound traffic-table index {PLAN_ID} outbound traffic-table index {PLAN_ID}"
             )
-            verifySPID(comm,command,data)
+            verifySPID(comm, command, data)
             log(
                 colorFormatter(
                     f"Al cliente {data['name']} {data['frame']}/{data['slot']}/{data['port']}/{data['id']} @ OLT {data['olt']} se le ha Cambiado el plan y vlan a {NEW_PLAN} @ {NEW_VLAN}",
                     "info",
                 )
             )
-            modify(data["sn"],NEW_PLAN,"PLAN")
-            modify(data["sn"],NEW_VLAN,"PROVIDER")
+            modify(data["sn"], NEW_PLAN, "PLAN")
+            modify(data["sn"], NEW_VLAN, "PROVIDER")
             quit()
             return
         NEW_PLAN = inp("Ingrese el Nuevo plana instalar : ")
         PLAN = plans[NEW_PLAN]
-        """
-        Apply a change to the srvProfile,Line Profile and gemport also deleting the existing spid
-        
-        steps:
-        1. get the clients's spid
-        2. delete the selected SPID
-        3. modify clients srv and lp profiles
-        4. generate a new spid with its specs
-        5. reconfigure wan port
-        """
         for wanData in data["wan"]:
             command(f"undo service-port {wanData['spid']}")
-            
+
         command(f"interface gpon {data['frame']}/{data['slot']}")
-        command(f"ont modify {data['port']} {data['id']} ont-lineprofile-id {PLAN['lineProfile']}")
-        command(f"ont modify {data['port']} {data['id']} ont-srvprofile-id {PLAN['srvProfile']}")
+        command(
+            f"ont modify {data['port']} {data['id']} ont-lineprofile-id {PLAN['lineProfile']}")
+        command(
+            f"ont modify {data['port']} {data['id']} ont-srvprofile-id {PLAN['srvProfile']}")
+        command(
+            f"ont ipconfig {data['port']} {data['id']} ip-index 2 dhcp vlan {PLAN['vlan']}"
+        )
         addVlan = inp("Se agregara vlan al puerto? [Y | N] : ")
         if addVlan == "Y":
             command(
@@ -104,17 +101,29 @@ $ """
             )
         command("quit")
         command(
-        f' service-port {data["wan"][0]["spid"]} vlan {PLAN["vlan"]} gpon {data["frame"]}/{data["slot"]}/{data["port"]} ont {data["id"]} gemport {PLAN["gemPort"]} multi-service user-vlan {PLAN["vlan"]} tag-transform transparent inbound traffic-table index {PLAN["plan"]} outbound traffic-table index {PLAN["plan"]}'
+            f' service-port {data["wan"][0]["spid"]} vlan {PLAN["vlan"]} gpon {data["frame"]}/{data["slot"]}/{data["port"]} ont {data["id"]} gemport {PLAN["gemPort"]} multi-service user-vlan {PLAN["vlan"]} tag-transform transparent inbound traffic-table index {PLAN["plan"]} outbound traffic-table index {PLAN["plan"]}'
         )
         sleep(10)
         command(f"interface gpon {data['frame']}/{data['slot']}")
-        command(f"ont wan-config {data['port']} {data['id']} ip-index 2 profile-id 0")
+        command(
+            f"ont wan-config {data['port']} {data['id']} ip-index 2 profile-id 0")
         command("quit")
+        verifySPID(comm, command, data)
+        log(
+            colorFormatter(
+                f"Al cliente {data['name']} {data['frame']}/{data['slot']}/{data['port']}/{data['id']} @ OLT {data['olt']} se le ha Cambiado el plan y vlan a {NEW_PLAN} @ {PLAN['vlan']}",
+                "info",
+            )
+        )
+        modify(data["sn"], NEW_PLAN, "PLAN")
+        modify(data["sn"], NEW_VLAN, "PROVIDER")
+        quit()
         return
     if action == "ES":
         log("| {:^3} | {:^4} | {:^6} |".format("IDX", "VLAN", "SPID"))
         for idx, wan in enumerate(data["wan"]):
-            log("| {:^3} | {:^4} | {:^6} |".format(idx, wan["vlan"], wan["spid"]))
+            log("| {:^3} | {:^4} | {:^6} |".format(
+                idx, wan["vlan"], wan["spid"]))
         DEL_SPID = int(inp("Ingrese el SPID a eliminar : "))
         command(f"undo service-port {DEL_SPID}")
         log(
@@ -140,9 +149,9 @@ Ingrese el tipo de servicio a instalar :
             log(f"SPID PARA AGG {data['spid']}, FUNCION AUN NO DISPONIBLE")
             quit()
             return
-        data["wan"][0]["spid"] = availableSpid(comm,command)
+        data["wan"][0]["spid"] = availableSpid(comm, command)
         addOnuService(comm, command, data)
-        verifySPID(comm,command,data)
+        verifySPID(comm, command, data)
         log(
             colorFormatter(
                 f"Al cliente {data['name']} {data['frame']}/{data['slot']}/{data['port']}/{data['id']} @ OLT {data['olt']} se le ha Agregado el plan y vlan a {data['plan']} @ {data['vlan']}",
