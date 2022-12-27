ed32cf349427030235352a168c6c1da724531b2b
Ricardo Vera <ricardovera71@gmail.com>
2022-12-27 15:04:39 -0400

    add(MC->CP 1): added CP module for new OLT

--- scripts/MC.py
+++ scripts/MC.py
@@ -1,9 +1,10 @@
+from time import sleep
 from helpers.clientFinder.dataLookup import dataLookup
 from helpers.operations.addHandler import addOnuService
 from helpers.operations.spid import availableSpid, spidCalc, verifySPID
 from helpers.utils.display import display
 from helpers.utils.printer import colorFormatter, inp, log
-from helpers.info.plans import oldPlans
+from helpers.info.plans import oldPlans, plans
 from helpers.utils.sheets import modify
 
 
@@ -78,6 +79,37 @@ $ """
             modify(data["sn"],NEW_VLAN,"PROVIDER")
             quit()
             return
+        NEW_PLAN = inp("Ingrese el Nuevo plana instalar : ")
+        PLAN = plans[NEW_PLAN]
+        """
+        Apply a change to the srvProfile,Line Profile and gemport also deleting the existing spid
+        
+        steps:
+        1. get the clients's spid
+        2. delete the selected SPID
+        3. modify clients srv and lp profiles
+        4. generate a new spid with its specs
+        5. reconfigure wan port
+        """
+        for wanData in data["wan"]:
+            command(f"undo service-port {wanData['spid']}")
+            
+        command(f"interface gpon {data['frame']}/{data['slot']}")
+        command(f"ont modify {data['port']} {data['id']} ont-lineprofile-id {PLAN['lineProfile']}")
+        command(f"ont modify {data['port']} {data['id']} ont-srvprofile-id {PLAN['srvProfile']}")
+        addVlan = inp("Se agregara vlan al puerto? [Y | N] : ")
+        if addVlan == "Y":
+            command(
+                f" ont port native-vlan {data['port']} {data['id']} eth 1 vlan {PLAN['vlan']} "
+            )
+        command("quit")
+        command(
+        f' service-port {data["wan"][0]["spid"]} vlan {PLAN["vlan"]} gpon {data["frame"]}/{data["slot"]}/{data["port"]} ont {data["id"]} gemport {PLAN["gemPort"]} multi-service user-vlan {PLAN["vlan"]} tag-transform transparent inbound traffic-table index {PLAN["plan"]} outbound traffic-table index {PLAN["plan"]}'
+        )
+        sleep(10)
+        command(f"interface gpon {data['frame']}/{data['slot']}")
+        command(f"ont wan-config {data['port']} {data['id']} ip-index 2 profile-id 0")
+        command("quit")
         return
     if action == "ES":
         log("| {:^3} | {:^4} | {:^6} |".format("IDX", "VLAN", "SPID"))
