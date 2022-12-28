f564136ffd22ed300a8913155b2418714a0d2be8
Ricardo Vera <ricardovera71@gmail.com>
2022-12-28 16:20:38 -0400

    delete(sheets): deleted func to add to drive data

--- helpers/utils/sheets.py
+++ helpers/utils/sheets.py
@@ -18,31 +18,9 @@ cellMap = {
     'STATE': "N",
 }
 
-cellMapNeli = {
-    '0': "C",
-    '1': "AB",
-    '2': "AC",
-    '3': "AD",
-    '4': "AE",
-    '5': "AF",
-    '6': "AJ",
-    '7': "AT",
-    '8': "AU",
-}
-
 gc = pygsheets.authorize(service_account_file=path)
 sh = gc.open("CPDC")
 wks = sh[0]
-neliSh = gc.open("CO Instalaciones Oz. y MDU")
-neliWks = neliSh[0]
-
-    
-def insertNeli(values):
-    vals = [values[2],values[3],values[4],values[5],values[6],values[7],values[0],values[14],f"{values[15]}°C"]
-    neli_lstRow = len(neliWks.get_all_records()) + 2
-    for idx,value in enumerate(vals):
-        neliWks.update_value(f"{cellMapNeli[f'{idx}']}{neli_lstRow}", value)
-    
 
 def modify(lookupVal, newVal, tp):
     cell = wks.find(lookupVal)[0]
@@ -59,4 +37,3 @@ def insert(values):
             vals.append(value)
     lst_row = len(wks.get_all_records())  + 1
     wks.insert_rows(lst_row, values=vals)
-    insertNeli(values)
