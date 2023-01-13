c32a44939fed1c22144bb4919cfc078b8b645efa
Ricardo Vera <ricardovera71@gmail.com>
2023-01-13 09:56:48 -0400

    feat(addHandler, RTR): improved adding temporal fix for adding clients in olt2 & olt3 | added funcitonality of ip conflict resolver

--- helpers/info/hashMaps.py
+++ helpers/info/hashMaps.py
@@ -4,6 +4,8 @@ devices = {
   "OLT1":"181.232.180.7",
   "OLT2":"181.232.180.5",
   "OLT3":"181.232.180.6",
-  "RTR1":{"ip":"181.232.180.1","ints":["GigabitEthernet0/3/7(10G)"]},
-  "RTR2":{"ip":"181.232.180.2","ints":["GigabitEthernet0/3/7(10G)", "GigabitEthernet0/3/8(10G)"]},
+  "RTRE1":{"ip":"181.232.180.1","ints":["GigabitEthernet0/3/7(10G)"]},
+  "RTRE2":{"ip":"181.232.180.2","ints":["GigabitEthernet0/3/7(10G)", "GigabitEthernet0/3/8(10G)"]},
+  "RTRA1":"181.232.180.3",
+  "RTRA2":"181.232.180.4",
 }
--- scripts/RTR.py
+++ scripts/RTR.py
@@ -7,40 +7,42 @@ from helpers.utils.ssh import ssh
 
 
 def rtr():
-    rtrTypes = ["1", "2"]
+    rtrTypes = ["e1", "e2", "a1", "a2"]
     ip = ""
     interfaces = []
     rt = inp(
-        "Selecciona el router de borde a monitorear [1 | 2] : ").upper()
+        "Selecciona el router a monitorear [E1 | E2 | A1 | A2] : ")
     if rt in rtrTypes:
         ip = devices[f"RTR{rt}"]["ip"]
         intList = devices[f"RTR{rt}"]["ints"]
         (comm, command, quit) = ssh(ip)
         decoder(comm)
-        for i in range(0, 5):
-            command("display interface brief | no-more")
-            value = decoder(comm)
-            for interface in intList:
-                interfaces.append(intFormatter(value, interface))
+        if "E" in rt:
+            for i in range(0, 5):
+                command("display interface brief | no-more")
+                value = decoder(comm)
+                for interface in intList:
+                    interfaces.append(intFormatter(value, interface))
 
-        log(
-            "| {:^25} | {:^4} | {:^8} | {:^8} | {:^8} | {:^8} | {:^9} |".format(
-                "Interface", "PHY", "Protocol", "InUti", "OutUti", "inErrors", "outErrors"
+            log(
+                "| {:^25} | {:^4} | {:^8} | {:^8} | {:^8} | {:^8} | {:^9} |".format(
+                    "Interface", "PHY", "Protocol", "InUti", "OutUti", "inErrors", "outErrors"
+                )
             )
-        )
 
-        for interface in interfaces:
-            color = "fail" if interface["PHY"] != 'up' or float(
-                interface["InUti"][:-1]) < 8.0 else "ok"
-            res = colorFormatter("| {:^25} | {:^4} | {:^8} | {:^8} | {:^8} | {:^8} | {:^9} |".format(
-                interface["Interface"], interface["PHY"], interface["Protocol"], interface[
-                    "InUti"], interface["OutUti"], interface["inErrors"], interface["outErrors"]
-            ), color)
-            log(res)
-        quit()
-
+            for interface in interfaces:
+                color = "fail" if interface["PHY"] != 'up' or float(
+                    interface["InUti"][:-1]) < 8.0 else "ok"
+                res = colorFormatter("| {:^25} | {:^4} | {:^8} | {:^8} | {:^8} | {:^8} | {:^9} |".format(
+                    interface["Interface"], interface["PHY"], interface["Protocol"], interface[
+                        "InUti"], interface["OutUti"], interface["inErrors"], interface["outErrors"]
+                ), color)
+                log(res)
+            quit()
+        if "A" in rt:
+            ""
     else:
         resp = colorFormatter(
-            f"No se puede Conectar al router de borde, Error Router {rt} no existe", "warning")
+            f"No se puede Conectar al router, Error Router {rt} no existe", "warning")
         log(resp)
         sleep(1)
