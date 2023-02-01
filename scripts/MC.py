a35fded33f0fc3e0a251fc97c1cb65445bb03e28
Ricardo Vera <ricardovera71@gmail.com>
2023-02-01 18:49:18 -0400

    fix(MC): not handling error

--- scripts/MC.py
+++ scripts/MC.py
@@ -33,92 +33,95 @@ $ """
     lookupType = inp(
         "Buscar cliente por serial o por Datos (F/S/P/ID) [S | D] : ")
     client = dataLookup(comm, command, olt, lookupType)
-    proceed = display(client, "A")
-    if not proceed:
-        log(colorFormatter("Cancelando...", "warning"))
-        quit()
-        return
-    if action == "CT":
-        NEW_NAME = inp("Ingrese el Nuevo Titular del cliente : ")
-        command(f"interface gpon {client['frame']}/{client['slot']}")
-        command(f'ont modify {client["port"]} {client["onu_id"]} desc "{NEW_NAME}" ')
-        command("quit")
-        log(
-            colorFormatter(
-                f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha cambiado el nombre a '{NEW_NAME}'",
-                "success",
+    if client["fail"] != None:
+        proceed = display(client, "A")
+        if not proceed:
+            log(colorFormatter("Cancelando...", "warning"))
+            quit()
+            return
+        if action == "CT":
+            NEW_NAME = inp("Ingrese el Nuevo Titular del cliente : ")
+            command(f"interface gpon {client['frame']}/{client['slot']}")
+            command(f'ont modify {client["port"]} {client["onu_id"]} desc "{NEW_NAME}" ')
+            command("quit")
+            log(
+                colorFormatter(
+                    f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha cambiado el nombre a '{NEW_NAME}'",
+                    "success",
+                )
             )
-        )
-        modify(client["sn"], NEW_NAME, "NAME")
-        quit()
-        return
-    if action == "CO":
-        NEW_SN = inp("Ingrese el Nuevo serial de ONT del cliente : ")
-        command(f"interface gpon {client['frame']}/{client['slot']}")
-        command(f'ont modify {client["port"]} {client["onu_id"]} sn "{NEW_SN}" ')
-        command("quit")
-        log(
-            colorFormatter(
-                f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha cambiado el serial a '{NEW_SN}'",
-                "success",
+            modify(client["sn"], NEW_NAME, "NAME")
+            quit()
+            return
+        if action == "CO":
+            NEW_SN = inp("Ingrese el Nuevo serial de ONT del cliente : ")
+            command(f"interface gpon {client['frame']}/{client['slot']}")
+            command(f'ont modify {client["port"]} {client["onu_id"]} sn "{NEW_SN}" ')
+            command("quit")
+            log(
+                colorFormatter(
+                    f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha cambiado el serial a '{NEW_SN}'",
+                    "success",
+                )
             )
-        )
-        modify(client["sn"], NEW_SN, "SN")
-        quit()
-        return
-    if action == "CP":
-        for wanData in client["wan"]:
-            command(f"undo service-port {wanData['spid']}")
-        client['plan_name'] = inp("Ingrese el Nuevo plan a instalar : ")
-        client["wan"][0] = PLANS[client["olt"]][client["plan_name"]]
-        command(f"interface gpon {client['frame']}/{client['slot']}")
-        command(f"ont modify {client['port']} {client['onu_id']} ont-lineprofile-id {client['wan'][0]['line_profile']}")
-        command(f"ont modify {client['port']} {client['onu_id']} ont-srvprofile-id {client['wan'][0]['srv_profile']}")
-        command("quit")
-        addOnuServiceNew(comm, command, client)
-        verifySPID(comm, command, client)
-        log(
-            colorFormatter(
-                f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha Cambiado el plan y vlan a {client['plan_name']} @ {client['wan'][0]['vlan']}",
-                "success",
+            modify(client["sn"], NEW_SN, "SN")
+            quit()
+            return
+        if action == "CP":
+            for wanData in client["wan"]:
+                command(f"undo service-port {wanData['spid']}")
+            client['plan_name'] = inp("Ingrese el Nuevo plan a instalar : ")
+            client["wan"][0] = PLANS[client["olt"]][client["plan_name"]]
+            command(f"interface gpon {client['frame']}/{client['slot']}")
+            command(f"ont modify {client['port']} {client['onu_id']} ont-lineprofile-id {client['wan'][0]['line_profile']}")
+            command(f"ont modify {client['port']} {client['onu_id']} ont-srvprofile-id {client['wan'][0]['srv_profile']}")
+            command("quit")
+            addOnuServiceNew(comm, command, client)
+            verifySPID(comm, command, client)
+            log(
+                colorFormatter(
+                    f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha Cambiado el plan y vlan a {client['plan_name']} @ {client['wan'][0]['vlan']}",
+                    "success",
+                )
             )
-        )
-        modify(client["sn"], client['plan_name'], "PLAN")
-        modify(client["sn"], client['wan'][0]['vlan'], "PROVIDER")
-        quit()
-        return
-    if action == "ES":
-        log("| {:^3} | {:^4} | {:^6} |".format("IDX", "VLAN", "SPID"))
-        for idx, wan in enumerate(client["wan"]):
-            log("| {:^3} | {:^4} | {:^6} |".format(
-                idx, wan["vlan"], wan["spid"]))
-        DEL_SPID = int(inp("Ingrese el SPID a eliminar : "))
-        command(f"undo service-port {DEL_SPID}")
-        log(
-            colorFormatter(
-                f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha eliminado el SPID {DEL_SPID}",
-                "info",
+            modify(client["sn"], client['plan_name'], "PLAN")
+            modify(client["sn"], client['wan'][0]['vlan'], "PROVIDER")
+            quit()
+            return
+        if action == "ES":
+            log("| {:^3} | {:^4} | {:^6} |".format("IDX", "VLAN", "SPID"))
+            for idx, wan in enumerate(client["wan"]):
+                log("| {:^3} | {:^4} | {:^6} |".format(
+                    idx, wan["vlan"], wan["spid"]))
+            DEL_SPID = int(inp("Ingrese el SPID a eliminar : "))
+            command(f"undo service-port {DEL_SPID}")
+            log(
+                colorFormatter(
+                    f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha eliminado el SPID {DEL_SPID}",
+                    "info",
+                )
             )
-        )
-        quit()
-        return
-    if action == "AS":
-        client['plan_name'] = inp("Ingrese el Nuevo plana instalar : ")
-        client["wan"][0] = PLANS[client["olt"]][client["plan_name"]]
-        addOnuServiceNew(comm, command, client)
-        verifySPID(comm, command, client)
-        log(
-            colorFormatter(
-                f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha Agregado el plan y vlan a {client['plan_name']} @ {client['wan'][0]['vlan']}",
-                "info",
+            quit()
+            return
+        if action == "AS":
+            client['plan_name'] = inp("Ingrese el Nuevo plana instalar : ")
+            client["wan"][0] = PLANS[client["olt"]][client["plan_name"]]
+            addOnuServiceNew(comm, command, client)
+            verifySPID(comm, command, client)
+            log(
+                colorFormatter(
+                    f"Al cliente {client['name']} {client['frame']}/{client['slot']}/{client['port']}/{client['onu_id']} @ OLT {client['olt']} se le ha Agregado el plan y vlan a {client['plan_name']} @ {client['wan'][0]['vlan']}",
+                    "info",
+                )
             )
-        )
-        quit()
-        return
-        
-    if action == "AV":
-        client["wan"][0]["spid"] = spidCalc(client)["V"]
-        log(colorFormatter(
-            f"SPID PARA AGG {client['spid']}, FUNCION AUN NO DISPONIBLE"), "info")
-        quit()
-        return
+            quit()
+            return
+            
+        if action == "AV":
+            client["wan"][0]["spid"] = spidCalc(client)["V"]
+            log(colorFormatter(
+                f"SPID PARA AGG {client['spid']}, FUNCION AUN NO DISPONIBLE"), "info")
+            quit()
+            return
+    else:
+        log(colorFormatter(client["fail"],"fail"))
