be8f11adbc5809828bedf56076e3c38472736986
Ricardo Vera <ricardovera71@gmail.com>
2023-02-08 13:15:51 -0400

    feat(ssh): added multiple sessions

--- helpers/utils/ssh.py
+++ helpers/utils/ssh.py
@@ -7,18 +7,31 @@ from helpers.utils.printer import colorFormatter, log
 
 load_dotenv()
 
-username = os.environ["user"]
-password = os.environ["password"]
-port = os.environ["port"]
 
 
 def ssh(ip, debugging):
+    count = 1
     delay = 1.5
     conn = paramiko.SSHClient()
     conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    conn.connect(ip, port, username, password)
-    comm = conn.invoke_shell()
+    comm = None
+    cont = True
 
+    # Handling multiple SSH sessions
+    while cont and count <= 3:
+        try:
+            username = os.environ[f"user_{count}"]
+            password = os.environ[f"password_{count}"]
+            port = os.environ["port"]
+            conn.connect(ip, port, username, password)
+            comm = conn.invoke_shell()
+            cont = False
+        except paramiko.ssh_exception.AuthenticationException:
+            cont = True
+            count += 1
+            continue
+        break
+    
     def enter():
         comm.send(" \n")
         comm.send(" \n")
@@ -43,4 +56,3 @@ def ssh(ip, debugging):
         command("sys")
 
     return (comm, command, quit)
-
