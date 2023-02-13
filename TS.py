640a7af83fbbc8d6dddb61bd612f63ff2faed2ff
Ricardo Vera <ricardovera71@gmail.com>
2023-02-12 22:55:51 -0400

    dev(TS)

--- TS.py
+++ TS.py
@@ -45,19 +45,59 @@ def compare():
     for oltClient in olt:
       if oltClient["sn"] == odooClient["Serial del ONT"]:
         client.append({
-          "name": odooClient["Cliente"],
-          "contract": str(odooClient["Referencia"]).zfill(10),
-          "ID externo": odooClient["ID.1"],
+          "fsp":f'{oltClient["frame"]}/{oltClient["slot"]}/{oltClient["port"]}',
           "frame":oltClient["frame"],
           "slot":oltClient["slot"],
           "port":oltClient["port"],
           "onu_id":oltClient["onu_id"],
-          "device":oltClient["device"],
+          "name":odooClient["Cliente"],
+          "contract": str(odooClient["Referencia"]).zfill(10),
           "sn":oltClient["sn"],
+          "device":oltClient["device"],
+          "ID externo": odooClient["ID.1"],
           "state":oltClient["state"],
-          "olt": "La Lago"
+          "olt": "1",
+          "plan_name":oltClient["plan_name"]
         })
   print("Selecciona la carpeta de resultados...")
   path = filedialog.askdirectory()
   dictToFile("RESULTADO", "E", path, client, False)
 compare()
+
+
+#################        SMART OLT REQUESTS         ##################
+
+# import requests
+
+# sub_domain = "conext"
+# api_key="6125bdf043c44912847385f9e62ee42d"
+
+
+# def activate_batch():
+#   url = f"https://{sub_domain}.smartolt.com/api/onu/bulk_enable"
+#   payload={
+#     "onus_external_ids":"9876543210"
+#   }
+#   headers = {
+#     'X-Token': f'{api_key}'
+#   }
+#   response = requests.request("POST", url, headers=headers, data=payload, files=[])
+#   print(response.text)
+
+# # activate_batch()
+
+# def deactivate_batch():
+#   url = f"https://{sub_domain}.smartolt.com/api/onu/bulk_disable"
+#   payload={
+#     "onus_external_ids":"9876543210"
+#   }
+#   headers = {
+#     'X-Token': f'{api_key}'
+#   }
+#   response = requests.request("POST", url, headers=headers, data=payload, files=[])
+#   print(response.text)
+
+# deactivate_batch()
+
+
+#################        SMART OLT REQUESTS         ##################
