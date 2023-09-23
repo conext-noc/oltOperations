8c0514e031e250c75b59e9cb2ac05b33d84fc172
Ricardo Vera <ricardovera71@gmail.com>
2023-09-22 19:23:02 -0500

    fix(namings)

--- helpers/handlers/display.py
+++ helpers/handlers/display.py
@@ -9,24 +9,24 @@ def display(data, tp):
     """
     proceed = False
     res = f"""
-|FRAME               :   {data["frame"]}
-|SLOT                :   {data["slot"]}
-|PORT                :   {data["port"]}
-|ONU_ID              :   {data["onu_id"]}
-|NAME                :   {f'{data["name_1"]} {data["name_2"]} {data["contract"]}'}
-|SN                  :   {data["sn"]}
-|CONTROL FLAG        :   {data["state"]}
-|RUN STATE           :   {data["status"]}
-|LAST DOWN CAUSE     :   {data.get("last_down_cause") or "N/A"}
-|LAST DOWN TIME      :   {data.get("last_down_time") or "N/A"}
-|LAST DOWN DATE      :   {data.get("last_down_date") or "N/A"}
-|ONT TYPE            :   {data["device"]}
-|TEMPERATURA         :   {data["temp"]}
-|POTENCIA            :   {data["pwr"]}
-|POTENCIA RX OLT     :   {data["pwr_rx"]}
-|VLAN                :   {data["vlan"]}
-|PLAN                :   {data["plan_name"]}
-|SPID                :   {data["spid"]}
+|FRAME                      :   {data["frame"]}
+|SLOT                       :   {data["slot"]}
+|PORT                       :   {data["port"]}
+|ONU_ID                     :   {data["onu_id"]}
+|NAME                       :   {f'{data["name_1"]} {data["name_2"]} {data["contract"]}'}
+|SN                         :   {data["sn"]}
+|CONTROL FLAG               :   {data["state"]}
+|RUN STATE                  :   {data["status"]}
+|LAST DOWN CAUSE            :   {data.get("last_down_cause") or "N/A"}
+|LAST DOWN TIME             :   {data.get("last_down_time") or "N/A"}
+|LAST DOWN DATE             :   {data.get("last_down_date") or "N/A"}
+|ONT TYPE                   :   {data["device"]}
+|TEMPERATURA                :   {data["temp"]}
+|POTENCIA DE RECEPCION ONT  :   {data["pwr"]}
+|POTENCIA DE RECEPCION OLT  :   {data["pwr_rx"]}
+|VLAN                       :   {data["vlan"]}
+|PLAN                       :   {data["plan_name"]}
+|SPID                       :   {data["spid"]}
             """
     log(res, "ok")
     val = inp("desea continuar? [Y|N] : ").upper() if tp == "A" else None
