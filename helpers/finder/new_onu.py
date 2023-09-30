e46f4c70c98f5426d5dd56dc772af4913f3d42f2
Ricardo Vera <ricardovera71@gmail.com>
2023-09-29 19:26:21 -0500

    remove(onu discover time)

--- helpers/finder/new_onu.py
+++ helpers/finder/new_onu.py
@@ -10,7 +10,6 @@ newCond = "---------------------------------------------------------------------
 newCondFSP = "F/S/P               : "
 newCondFSPEnd = "ONT NNI type"
 newCondSn = "Ont SN              : "
-newCondTime = "Ont autofind time   : "
 
 
 def new_lookup(comm, command, SN_NEW):
@@ -26,46 +25,20 @@ def new_lookup(comm, command, SN_NEW):
         (e, _) = regex[ont + 1]
         result = value[s:e]
         (_, sFSP) = check(result, newCondFSP).span()
-        # (eFSP, _) = check(result, newCondFSPEnd).span()
         (_, eSN) = check(result, newCondSn).span()
-        (_, eT) = check(result, newCondTime).span()
         aSN = result[eSN : eSN + 16].replace("\n", "").replace(" ", "")
         aFSP = result[sFSP : sFSP + 7].replace("\n", "").replace(" ", "")
-        aT = result[eT : eT + 19].replace("\n", "")
-        t1 = datetime.strptime(aT, "%Y-%m-%d %H:%M:%S")
-        t2 = datetime.fromisoformat(str(datetime.now()))
-        clientTime = t2 - t1
         client.append(
             {
                 "fsp": aFSP.replace("\r", ""),
                 "sn": aSN,
-                "idx": ont + 1,
-                "time": clientTime.days,
+                "idx": ont + 1
             }
         )
     log(f"| {'IDX':^3} | {'F/S/P':^6} | {'SN':^16} |", "normal")
 
     for ont in client:
-        count = []
-        if SN_NEW == ont["sn"] and ont["time"] <= 10:
+        if SN_NEW == ont["sn"]:
             SN_FINAL = ont["sn"]
             FSP_FINAL = ont["fsp"]
-            log(f"| {ont['idx']:^3} | {ont['fsp']:^6} | {ont['sn']:^16} |", "success")
-            count.append({"sn": SN_FINAL, "fsp": FSP_FINAL})
-        elif SN_NEW == ont["sn"] and ont["time"] > 10:
-            log(f"| {ont['idx']:^3} | {ont['fsp']:^6} | {ont['sn']:^16} |", "warning")
-        else:
-            log(f"| {ont['idx']:^3} | {ont['fsp']:^6} | {ont['sn']:^16} |", "normal")
-        if len(count) > 1:
-            log(f"| {'IDX':^3} | {'F/S/P':^6} | {'SN':^16} |", "normal")
-            res = {}
-            for idx in count:
-                res[idx] = {"sn": ont["sn"], "fsp": ont["fsp"]}
-                log(
-                    f"| {idx:^3} | {ont['fsp']:^6} | {ont['sn']:^16} |",
-                    "normal",
-                )
-            ix = inp("SELECCIONE EL INDEX DEL SERIAL A UTILIZAR : ")
-            SN_FINAL = res[ix]["sn"]
-            FSP_FINAL = res[ix]["fsp"]
     return (SN_FINAL, FSP_FINAL)
