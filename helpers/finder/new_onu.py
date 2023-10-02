63b3c855c9a36c8d4b14a6c7f226664b060bcc20
Ricardo Vera <ricardovera71@gmail.com>
2023-10-02 09:48:17 -0500

    fix(new_lookup)

--- helpers/finder/new_onu.py
+++ helpers/finder/new_onu.py
@@ -25,20 +25,30 @@ def new_lookup(comm, command, SN_NEW):
         (e, _) = regex[ont + 1]
         result = value[s:e]
         (_, sFSP) = check(result, newCondFSP).span()
+        # (eFSP, _) = check(result, newCondFSPEnd).span()
         (_, eSN) = check(result, newCondSn).span()
         aSN = result[eSN : eSN + 16].replace("\n", "").replace(" ", "")
         aFSP = result[sFSP : sFSP + 7].replace("\n", "").replace(" ", "")
-        client.append(
-            {
-                "fsp": aFSP.replace("\r", ""),
-                "sn": aSN,
-                "idx": ont + 1
-            }
-        )
-    log(f"| {'IDX':^3} | {'F/S/P':^6} | {'SN':^16} |", "normal")
+        client.append({"fsp": aFSP.replace("\r", ""), "sn": aSN, "idx": ont + 1})
+    log("| {:^3} | {:^6} | {:^16} |".format("IDX", "F/S/P", "SN"), "normal")
 
     for ont in client:
+        count = []
         if SN_NEW == ont["sn"]:
             SN_FINAL = ont["sn"]
             FSP_FINAL = ont["fsp"]
+            log(
+                "| {:^3} | {:^6} | {:^16} |".format(ont["idx"], ont["fsp"], ont["sn"]),
+                "success",
+            )
+            count.append({"sn": SN_FINAL, "fsp": FSP_FINAL})
+        else:
+            log("| {:^3} | {:^6} | {:^16} |".format(ont["idx"], ont["fsp"], ont["sn"]), "normal")
+        if len(count) > 1:
+            log("| {:^3} | {:^6} | {:^16} |".format("IDX", "F/S/P", "sn"), "normal")
+            for idx, res in enumerate(count):
+                log("| {:^3} | {:^6} | {:^16} |".format(idx, res["fsp"], res["sn"]))
+            ix = inp("SELECCIONE EL INDEX DEL SERIAL A UTILIZAR : ")
+            SN_FINAL = res[ix]["sn"]
+            FSP_FINAL = res[ix]["fsp"]
     return (SN_FINAL, FSP_FINAL)
