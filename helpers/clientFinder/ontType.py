a75c4f91e2e400a7bf9cb0e9e196792c7ab67ffd
CesarSanch1998 <cs96548@gmail.com>
2023-11-30 10:46:26 -0400

    fix:BDCM query failure

--- helpers/clientFinder/ontType.py
+++ helpers/clientFinder/ontType.py
@@ -1,8 +1,10 @@
 from helpers.utils.decoder import check, decoder
 from helpers.failHandler.fail import failChecker
+import re
 
 ontTypeStart = "OntProductDescription    : "
 ontTypeEnd = "GPON"
+vendorId = r"Vendor-ID\s*:\s*(\w+)"
 
 def typeCheck(comm,command,data):
   ONT_TYPE = None
@@ -13,9 +15,15 @@ def typeCheck(comm,command,data):
   value = decoder(comm)
   FAIL = failChecker(value)
   if(FAIL == None):
-    (_,tS) = check(value,ontTypeStart).span()
-    (tE,_) = check(value, ontTypeEnd).span()
-    ONT_TYPE = value[tS:tE]
+    matches = re.findall(vendorId, value)
+    if matches:
+        if matches[0].strip() == "BDCM":
+            ONT_TYPE = matches[0].strip()
+        else:
+            (_,tS) = check(value,ontTypeStart).span()
+            (tE,_) = check(value, ontTypeEnd).span()
+            ONT_TYPE = value[tS:tE]
+        
     return ONT_TYPE
   else:
     return ONT_TYPE
