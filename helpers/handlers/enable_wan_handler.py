815c3c8b7a700641179d9c6481b91719364ef89c
Ricardo Vera <ricardovera71@gmail.com>
2023-12-08 13:20:24 -0600

    fix(enable_wan)

--- helpers/handlers/enable_wan_handler.py
+++ helpers/handlers/enable_wan_handler.py
@@ -6,4 +6,4 @@ def enable_wan(command, client):
     enable = inp("Desea habilitar el puerto wan? [y/N] : ")
     if "Y" == enable:
         command("diagnose")
-        command(f"ont wan-access http {client['frame']}/{client['slot']} {client['port']} {client['onu_id']}")
+        command(f"ont wan-access http {client['frame']}/{client['slot']} {client['port']} {client['onu_id']} enable")
