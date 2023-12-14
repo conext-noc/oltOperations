705a0741a1981c6fc461738f885f642ec50a1545
Ricardo Vera <ricardovera71@gmail.com>
2023-12-14 13:32:38 -0600

    fix(timeout handler)

--- helpers/utils/ssh.py
+++ helpers/utils/ssh.py
@@ -21,8 +21,8 @@ def ssh(ip, debugging):
             username = creds["data"][count]["user_name"]
             password = creds["data"][count]["password"]
             port = 22
-            conn.connect(ip, port, username, password)
             log(f"trying to connect with {username} @ {ip}", "info")
+            conn.connect(ip, port, username, password)
             comm = conn.invoke_shell()
             cont = False
         except paramiko.ssh_exception.AuthenticationException or TimeoutError:
