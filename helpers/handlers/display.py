c37bebc05aa40b1fec1faf35c8f9119544445f02
Ricardo Vera <ricardovera71@gmail.com>
2024-02-26 20:04:11 -0600

    add(template): error prevention

--- helpers/handlers/display.py
+++ helpers/handlers/display.py
@@ -9,26 +9,26 @@ def display(data, tp):
     """
     proceed = False
     res = f"""
-|FRAME                      :   {data["frame"]}
-|SLOT                       :   {data["slot"]}
-|PORT                       :   {data["port"]}
-|ONU_ID                     :   {data["onu_id"]}
-|NAME                       :   {f'{data["name_1"]} {data["name_2"]} {data["contract"]}'}
-|SN                         :   {data["sn"]}
-|CONTROL FLAG               :   {data["state"]}
-|RUN STATE                  :   {data["status"]}
-|DIRECCION IP               :   {data["ip"]}
-|MASCARA DE SUBRED          :   {data["mask"]}
-|LAST DOWN CAUSE            :   {data.get("last_down_cause") or "N/A"}
-|LAST DOWN TIME             :   {data.get("last_down_time") or "N/A"}
-|LAST DOWN DATE             :   {data.get("last_down_date") or "N/A"}
-|ONT TYPE                   :   {data["device"]}
-|TEMPERATURA                :   {data["temp"]}
-|POTENCIA DE RECEPCION ONT  :   {data["pwr"]}
-|POTENCIA DE RECEPCION OLT  :   {data["pwr_rx"]}
-|VLAN                       :   {data["vlan"]}
-|PLAN                       :   {data["plan_name"]}
-|SPID                       :   {data["spid"]}
+|FRAME                      :   {data.get("frame", "N/A")}
+|SLOT                       :   {data.get("slot", "N/A")}
+|PORT                       :   {data.get("port", "N/A")}
+|ONU_ID                     :   {data.get("onu_id", "N/A")}
+|NAME                       :   {f'{data.get("name_1", "N/A")} {data.get("name_2", "N/A")} {data.get("contract", "N/A")}'}
+|SN                         :   {data.get("sn", "N/A")}
+|CONTROL FLAG               :   {data.get("state", "N/A")}
+|RUN STATE                  :   {data.get("status", "N/A")}
+|DIRECCION IP               :   {data.get("ip", "N/A")}
+|MASCARA DE SUBRED          :   {data.get("mask", "N/A")}
+|LAST DOWN CAUSE            :   {data.get("last_down_cause", "N/A")}
+|LAST DOWN TIME             :   {data.get("last_down_time", "N/A")}
+|LAST DOWN DATE             :   {data.get("last_down_date", "N/A")}
+|ONT TYPE                   :   {data.get("device", "N/A")}
+|TEMPERATURA                :   {data.get("temp", "N/A")}
+|POTENCIA DE RECEPCION ONT  :   {data.get("pwr", "N/A")}
+|POTENCIA DE RECEPCION OLT  :   {data.get("pwr_rx", "N/A")}
+|VLAN                       :   {data.get("vlan", "N/A")}
+|PLAN                       :   {data.get("plan_name", "N/A")}
+|SPID                       :   {data.get("spid", "N/A")}
             """
     log(res, "ok")
     val = inp("desea continuar? [Y|N] : ").upper() if tp == "A" else None
--- helpers/handlers/template.py
+++ helpers/handlers/template.py
@@ -3,50 +3,50 @@ from helpers.handlers.printer import log
 
 def approved(data):
     template = f"""
-    |{data["name_1"]} {data["name_2"]} {data["contract"]}  |  {data['frame']}/{data['slot']}/{data['port']}/{data['onu_id']} 
-    |OLT  {data['olt']}  {data["wan"][0]["provider"]}  {data["plan_name"]}
-    |TEMPERATURA :   {data['temp']}
-    |POTENCIA    :   {data['pwr']}
-    |SN          :   {data['sn']}
-    |SPID        :   {data["wan"][0]["spid"]}"""
+    |{data.get("name_1", "N/A")} {data.get("name_2", "N/A")} {data.get("contract", "N/A")}  |  {data.get('frame', "N/A")}/{data.get('slot', "N/A")}/{data.get('port', "N/A")}/{data.get('onu_id', "N/A")} 
+    |OLT  {data.get('olt', "N/A")}  {data.get("wan", [{"provider":"N/A"}])[0]["provider"]}  {data.get("plan_name", "N/A")}
+    |TEMPERATURA :   {data.get('temp', "N/A")}
+    |POTENCIA    :   {data.get('pwr', "N/A")}
+    |SN          :   {data.get('sn', "N/A")}
+    |SPID        :   {data.get("wan", [{"spid":"N/A"}])[0]["spid"]}"""
     log(template, "success")
     return [
-        data["sn"],
-        f'{data["name_1"]} {data["name_2"]} {data["contract"]}',
-        data["olt"],
-        data["frame"],
-        data["slot"],
-        data["port"],
-        data["onu_id"],
-        data["device"],
+        data.get("sn"),
+        f'{data.get("name_1")} {data.get("name_2")} {data.get("contract")}',
+        data.get("olt"),
+        data.get("frame"),
+        data.get("slot"),
+        data.get("port"),
+        data.get("onu_id"),
+        data.get("device"),
         "active",
-        data["wan"][0]["provider"],
-        data["plan_name"],
-        data["wan"][0]["spid"],
+        data.get("wan",[{"provider":"N/A"}])[0]["provider"],
+        data.get("plan_name"),
+        data.get("wan", [{"spid":"N/A"}])[0]["spid"],
         "used",
     ]
 
 
 def denied(data, reason):
     template = f"""
-    |{data['name_1']}  |  {data['frame']}/{data['slot']}/{data['port']}/{data['onu_id']} 
-    |OLT  {data['olt']}  {data["provider"]}  {data["plan_name"]}
-    |TEMPERATURA :   {data['temp']}
-    |POTENCIA    :   {data['pwr']}
-    |SN          :   {data['sn']}
+    {data.get("name_1", "N/A")} {data.get("name_2", "N/A")} {data.get("contract", "N/A")}  |  {data.get('frame', "N/A")}/{data.get('slot', "N/A")}/{data.get('port', "N/A")}/{data.get('onu_id', "N/A")}
+    |OLT  {data.get('olt', "N/A")}  {data.get("provider", "N/A")} {data.get("plan_name", "N/A")}
+    |TEMPERATURA :   {data.get('temp', "N/A")}
+    |POTENCIA    :   {data.get('pwr', "N/A")}
+    |SN          :   {data.get('sn', "N/A")}
     |RAZÓN       :   {reason}"""
     log(template, "warning")
 
 
 def approvedDis(data):
     template = f"""
-    |{f'{data["name_1"]} {data["name_2"]} {data["contract"]}'} 
-    |{data['frame']}/{data['slot']}/{data['port']}/{data['onu_id']} 
-    |OLT  {data['olt']}  {data["provider"]}  {data["plan_name"]}
-    |TEMPERATURA :   {data['temp']}
-    |POTENCIA    :   {data['pwr']}
-    |SN          :   {data['sn']}
-    |SPID        :   {data["spid"]}"""
+    |{f'{data.get("name_1", "N/A")} {data.get("name_2", "N/A")} {data.get("contract", "N/A")}'} 
+    |{data.get('frame', "N/A")}/{data.get('slot', "N/A")}/{data.get('port', "N/A")}/{data.get('onu_id', "N/A")} 
+    |OLT  {data.get('olt', "N/A")}  {data.get("provider", "N/A")}  {data.get("plan_name", "N/A")}
+    |TEMPERATURA :   {data.get('temp', "N/A")}
+    |POTENCIA    :   {data.get('pwr', "N/A")}
+    |SN          :   {data.get('sn', "N/A")}
+    |SPID        :   {data.get("spid", "N/A")}"""
     log(template, "success")
 
 
@@ -64,11 +64,11 @@ def change(data, changeType, newVal):
     |{}/{}/{}/{}
     |se le ha {} '{}'
     """.format(
-        f'{data["name_1"]} {data["name_2"]} {data["contract"]}',
-        data["frame"],
-        data["slot"],
-        data["port"],
-        data["onu_id"],
+        f'{data.get("name_1", "N/A")} {data.get("name_2", "N/A")} {data.get("contract", "N/A")}',
+        data.get("frame", "N/A"),
+        data.get("slot", "N/A"),
+        data.get("port", "N/A"),
+        data.get("onu_id", "N/A"),
         types[changeType],
         newVal,
     )
